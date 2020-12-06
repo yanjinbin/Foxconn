eaa4a1041c15ecd5c4dc9aa44d5a9fb7599475fa
mammthon <yanjinbin@gmail.com>
2020-12-06 17:48:03 +0800

    排列组合

---
+++ Leetcode/L46.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int> nums) {
+        const int N = nums.size();
+        vector<vector<int>> ans;
+        vector<bool> used(N, false);
+        vector<int> path;
+        function<void(int)> dfs = [&](int cur) {
+            if (cur == N) {
+                ans.push_back(path);
+                return;
+            }
+            for (int i = 0; i < N; i++) {
+                if (used[i])continue;
+                used[i] = true;
+                path.push_back(nums[i]);
+                dfs(cur + 1);
+                path.pop_back();
+                used[i] = false;
+            }
+        };
+        dfs(0);
+        return ans;
+    }
+};
+
+int main() {
+    Solution INSTANCE;
+    vector<int> nums = {1, 2, 3, 4};
+    vector<vector<int>> ans = INSTANCE.permute(nums);
+    FOR(i, 0, ans.size()) {
+        FOR(j, 0, ans[i].size()) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
---
+++ Leetcode/L77.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+#include <iostream>
+
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        function<void(int)> dfs = [&](int s) {
+            if (cur.size() == k) {
+                ans.push_back(cur);
+                return;
+            }
+            for (int i = s; i < n; i++) {
+                cur.push_back(i);
+                dfs(i + 1);
+                cur.pop_back();
+            }
+        };
+        dfs(0);
+        return ans;
+    }
+
+    vector<vector<int>> combine02(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        dfs(1, n, ans, cur, k);
+        return ans;
+    }
+
+    void dfs(int cur, int n, vector<vector<int>> &ans, vector<int> &tmp, int k) {
+        if (tmp.size() == k) {
+            ans.push_back(tmp);
+            return;
+        }
+        for (int i = cur; i <= n; i++) {
+            tmp.push_back(i);
+            dfs(i + 1, n, ans, tmp, k);
+            tmp.pop_back();
+        }
+    }
+};
+
+int main() {
+    Solution INSTANCE;
+    vector<vector<int>> ans = INSTANCE.combine02(5, 3);
+    vector<vector<int>> res = INSTANCE.combine(5, 3);
+    FOR(i, 0, ans.size()) {
+        FOR(j, 0, ans[i].size()) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+

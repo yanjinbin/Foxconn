395fc578f83e0840bf7e3273e9931c831d4c06b8
yanjinbin <Yjb!6518680>
2022-01-31 22:25:04 +0800

    L2115 找到所有菜谱 拓扑排序

--- Leetcode/L2115.cpp
+++ Leetcode/L2115.cpp
@@ -3,9 +3,11 @@
 #include <functional>
 #include <iostream>
 #include <map>
+#include <set>
 #include <queue>
 #include <stack>
 #include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
@@ -23,8 +25,40 @@ const int MAX_VALUE = 0x7FFFFFFF, MIN_VALUE = 0x80000000, INF = 0x3F3F3F3F, kMod
 #define FI first
 #define SE second
 
+// Topology sort
 class Solution {
 public:
   vector<string>
-  findAllRecipes(vector<string> &recipes, vector<vector<string>> &ingredients, vector<string> &supplies) {}
+  findAllRecipes(vector<string> &recipes, vector<vector<string>> &ingredients, vector<string> &supplies) {
+    unordered_map<string, vector<string>> G;
+    unordered_map<string, int> indegree;
+    for (int i = 0; i < recipes.size(); i++) {
+      string v = recipes[i];
+      for (int j = 0; j < ingredients[i].size(); j++) {
+        string u = ingredients[i][j];
+        G[u].push_back(v);
+        indegree[v] += 1;
+      }
+    }
+    queue<string> q;
+    for (string s : supplies) {
+      q.push(s);
+    }
+    unordered_set<string> wanted(recipes.begin(), recipes.end());
+    vector<string> ans;
+    while (!q.empty()) {
+      string cur = q.front();
+      q.pop();
+      if (wanted.find(cur) != wanted.end()) {
+        ans.push_back(cur);
+      }
+      for (auto x : G[cur]) {
+        indegree[x] -= 1;
+        if (indegree[x] == 0) {
+          q.push(x);
+        }
+      }
+    }
+    return ans;
+  }
 };

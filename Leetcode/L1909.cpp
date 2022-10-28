8ef5a2e7bb981b44e794e2d44e43304c890a7446
yanjinbin <yanjinbin@gmail.com>
2022-10-29 00:50:16 +0800

    L1909 是否严格单挑递增

---
+++ Leetcode/L1909.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+const int MAX_VALUE = 0x7FFFFFFF, MIN_VALUE = 0x80000000, INF = 0x3F3F3F3F, kMod = 1E9 + 7;
+#define ready ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define debug puts("⚡️⚡️ bling bling ⚡️⚡️");
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define pii pair<int, int>
+#define LL long long
+#define LD long double
+#define PB push_back
+#define EB emplace_back
+#define MP make_pair
+#define FI first
+#define SE second
+
+// https://bit.ly/3DmP5JD
+class Solution {
+public:
+  bool canBeIncreasing(vector<int> &nums) {
+    for (int i = 0; i + 1 < nums.size(); i++) {
+      if (nums[i] >= nums[i + 1]) {
+        return strictIncr(nums, i) || strictIncr(nums, i + 1);
+      }
+    }
+    return true;
+  }
+  bool strictIncr(vector<int> nums, int idx) {
+    int cur = -INF;
+    for (int i = 0; i < nums.size(); i++) {
+      if (i == idx)
+        continue;
+      if (cur >= nums[i])
+        return false;
+      cur = nums[i];
+    }
+    return true;
+  }
+};

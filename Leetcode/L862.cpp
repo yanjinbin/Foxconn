54d6735d8829d2d9a994094f4de99b0876ed8f2e
yanjinbin <yanjinbin@gmail.com>
2022-10-29 11:58:20 +0800

    L862 和至少为 K 的最短子数组  单调双端队列

--- Leetcode/L862.cpp
+++ Leetcode/L862.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <stack>
-#include <set>
-#include <unordered_set>
-#include <map>
-#include <unordered_map>
 #include <algorithm>
-#include <functional>
 #include <cmath>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 using namespace std;
 const int MAX_VALUE = 0x7FFFFFFF, MIN_VALUE = 0x80000000, INF = 0x3F3F3F3F, kMod = 1E9 + 7;
-#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-#define debug puts("pigtoria bling bling ⚡️⚡️");
+#define ready ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+#define debug puts("⚡️⚡️bling bling ⚡️⚡️");
 #define FOR(i, a, b) for (int i = a; i < b; i++)
 #define pii pair<int, int>
 #define LL long long
@@ -28,26 +28,26 @@ const int MAX_VALUE = 0x7FFFFFFF, MIN_VALUE = 0x80000000, INF = 0x3F3F3F3F, kMod
 // time: O(N)
 class Solution {
 public:
-    int shortestSubarray(vector<int> &A, int K) {
-        int N = A.size();
-        vector<long> pre(N + 1, 0);
-        for (int i = 0; i < N; ++i) {
-            pre[i + 1] = pre[i] + (long) A[i];
-        }
-        int ans = N + 1; // IMPOSSIBLE
-        deque<int> monoq; //  mono incr
-        for (int k = 0; k <= N; ++k) {
-            // 维护单调递增
-            while (!monoq.empty() && pre[k] <= pre[monoq.back()]) {
-                monoq.pop_back();
-            }
-            // 移除头部元素 破坏子数组和>=K条件
-            while (!monoq.empty() && pre[k] >= pre[monoq.front()] + K) {
-                ans = min(ans, k - monoq.front());
-                monoq.pop_front();
-            }
-            monoq.push_back(k);
-        }
-        return ans < N + 1 ? ans : -1;
+  int shortestSubarray(vector<int> &A, int K) {
+    int N = A.size();
+    vector<long> preSum(N + 1, 0);
+    for (int i = 0; i < N; ++i) {
+      preSum[i + 1] = preSum[i] + (long)A[i];
     }
+    int ans = N + 1;  // IMPOSSIBLE
+    deque<int> incrQ; //  mono incr
+    for (int k = 0; k <= N; ++k) {
+      // 维护单调递增
+      while (!incrQ.empty() && preSum[k] <= preSum[incrQ.back()]) {
+        incrQ.pop_back();
+      }
+      // 移除头部元素 破坏子数组和>=K条件
+      while (!incrQ.empty() && preSum[k] - preSum[incrQ.front()] >= K) {
+        ans = min(ans, k - incrQ.front());
+        incrQ.pop_front();
+      }
+      incrQ.push_back(k);
+    }
+    return ans < N + 1 ? ans : -1;
+  }
 };
